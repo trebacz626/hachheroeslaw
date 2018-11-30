667c2e09e53d58a17901c1428fbed26cfb7f331e
KacperTr\Lenovo Flex3 <trebacz.kacper@gmail.com>
2018-11-30 15:44:33 +0100

    log/register site

--- src/http/views/main_page/log.html.js
+++ src/http/views/main_page/log.html.js
@@ -1,37 +1,31 @@
 ﻿var logpage = `
-<div>
+<div class="reg">
     <form onsubmit="authController.logIn(this);return false;" id="logblock">
         <div class="container">
-                <label for='email' name:"email"> <b>E-mail</b></label>
+                <label for='email' name:"email"> <b>E-mail</b></label><br>
                 <input type="email" placeholder="Wpisz email" name="email"  required>
-
-                <label for='password' name="password"><b>Hasło</b></label>
+                <br>
+                <label for='password' name="password"><b>Hasło</b></label><br>
                 <input type="password" placeholder="Wpisz hasło" name="password"  required>
-
+                <br>
                 <button type="submit" name='login'>Zaloguj się</button>
         </div>
-
     </form>
 </div>
-
 `;
 var regpage = `
-<div>
-       
+<div >  
     <form onsubmit="authController.register(this);return false;" id="regblock">
-            <div class="container">
-
-                <label for='email' name:"email"> <b>E-mail</b></label>
+            <div class="container" >
+                <label for='email' name:"email"> <b>E-mail</b></label><br>
                 <input type="email" placeholder="Wpisz email" name="email"  required>
-
-
-                <label for='name'> <b>Nazwa użytkownika</b></label>
+                <br>
+                <label for='name'> <b>Nazwa użytkownika</b></label><br>
                 <input type="name" placeholder="Wpisz nazwę użytkownika" name="name"  required>
-
-
-                <label for='password' name="password"><b>Hasło</b></label>
+                <br>
+                <label for='password' name="password"><b>Hasło</b></label><br>
                 <input type="password" placeholder="Wpisz hasło" name="password"  required>
-
+                <br>
                 <button type="submit" name='register'>Zarejestruj się</button>
             </div>
 
